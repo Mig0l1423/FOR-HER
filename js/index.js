d3709f5445dd3778217ba1cc4a5c768f1d646eef
Mig0l1423 <miguellagao8@gmail.com>
2024-11-17 23:09:19 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'FOR YOU'.split('')
+const text = 'FOR YOUUUUU'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
