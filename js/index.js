b39e151a8f19af67915522436807bdf185dc2763
Mig0L23 <miguellagao8@gmail.com>
2024-11-18 00:44:12 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'FOR YOUUUUU'.split('')
+const text = 'I’ve Got A Little Something Special Just For U'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
